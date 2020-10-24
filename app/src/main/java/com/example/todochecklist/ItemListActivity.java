3030c0cce38406b81e159ebd235e19f0f6297e9e
naga-naga <z8pri2hr2ezjx0pith4e@docomo.ne.jp>
2020-10-24 21:02:00 +0900

    チェックリストの名前変更機能追加（表示のみ）

--- app/src/main/java/com/example/todochecklist/ItemListActivity.java
+++ app/src/main/java/com/example/todochecklist/ItemListActivity.java
@@ -1,30 +1,39 @@
 package com.example.todochecklist;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class ItemListActivity extends AppCompatActivity {
     private String checklistName;
+    private String newChecklistName;
     private ItemLister itemLister;
     private ItemListsManager itemListsManager;
+    private TextView checklistNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_itemlist);
 
+        newChecklistName = "";
+
         Intent intent = getIntent();
         checklistName = intent.getStringExtra(ChecklistLister.CHECKLIST_NAME);
 
         itemListsManager = new ItemListsManager(this);
 
-        TextView textView = (TextView)findViewById(R.id.itemlist_checklist_name);
-        textView.setText(checklistName);
+        checklistNameTextView = (TextView)findViewById(R.id.itemlist_checklist_name);
+        checklistNameTextView.setText(checklistName);
 
         ListView listView = (ListView)findViewById(R.id.itemlist_list);
         itemLister = new ItemLister(this, listView, itemListsManager, checklistName);
@@ -49,6 +58,36 @@ public class ItemListActivity extends AppCompatActivity {
     protected void onStart(){
         super.onStart();
         itemLister.updateListView();
+    }
+
+    public void renameChecklistName(View view){
+        final View renameView = getLayoutInflater().inflate(R.layout.checklist_adder, null, false);
+        final TextInputEditText textInputEditText = (TextInputEditText)renameView.findViewById(R.id.checklist_adder_text);
+        textInputEditText.setText(checklistName);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.rename_checklist_title)
+                .setView(renameView)
+                .setPositiveButton(R.string.rename,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                newChecklistName = textInputEditText.getText().toString();
+
+                                if(!newChecklistName.equals("")){
+                                    checklistNameTextView.setText(newChecklistName);
+                                }
+                            }
+                        })
+                .setNegativeButton(R.string.cancel,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Nothing to do
+                            }
+                        })
+                .create()
+                .show();
     }
 
     public void addItem(View view){
